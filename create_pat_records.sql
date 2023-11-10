805306805dcc001a12cfdb5172dc1e815e729934
Baker Renneckar <brenneckar@collegetrack.org>
2023-11-10 11:27:55 -0800

    updating PAT creation process for advising rubric updates

--- create_pat_records.sql
+++ create_pat_records.sql
@@ -3,8 +3,11 @@ WITH
   SELECT
     -- POPULATE THESE EVERY UPDATE
     "Spring" AS term_to_create,
-    "2022-23" AS year_to_create,
+    "2023-24" AS year_to_create,
+    date(2024,01,01) as start_date_of_term_to_create,
+
     -- END MANAUL UPDATE
+    case when at_name like '%(Semester)%' then "Semester" when at_name like '%(Quarter)%' then 'Quarter' else 'Check' end as school_calendar,
     contact_id,
     at_id AS previous_academic_semester_c,
     GAS_Name AS current_gas_name,
@@ -21,24 +24,19 @@ WITH
     cumulative_credits_awarded_most_recent_c,
     persistence_at_prev_enrollment_status_c,
     '01246000000RNnHAAW' AS record_type_id,
-    -- Advising Rubric Fields to Cary Over Term to Term
+    -- Advising Rubric Fields to Cary Over
     financial_aid_package_c,
-    free_checking_account_c,
-    e_fund_c,
     repayment_plan_c,
+    repayment_policies_c,
     loan_exit_c,
     academic_networking_50_cred_c,
     academic_networking_over_50_credits_c,
-    extracurricular_activity_c,
-    finding_opportunities_75_c,
-    resume_cover_letter_c,
-    career_counselor_25_credits_c,
-    career_field_2550_credits_c,
-    resources_2550_credits_c,
+    exploration_career_c,
     internship_5075_credits_c,
-    post_graduate_plans_5075_creds_c,
     post_graduate_opportunities_75_cred_c,
+    interested_in_graduate_school_50_credits_c,
     alumni_network_75_credits_c
+
   FROM
     `data-studio-260217.prod_core.contact_at_template`
   WHERE
@@ -53,7 +51,8 @@ WITH
     academic_year_c,
     GAS_Name,
     term_c,
-    academic_calendar_category_c
+    academic_calendar_category_c,
+    gas_start_date,
   FROM
     `data-studio-260217.prod_staging.stg_salesforce__global_academic_semester_c` ),
   join_term_ids AS (
@@ -61,13 +60,15 @@ WITH
     gather_current_term_data.*,
     term_ids.GAS_id,
     term_ids.academic_year_c,
-    term_ids.GAS_Name AS gas_name_to_create
+    term_ids.GAS_Name AS gas_name_to_create,
+
+  
   FROM
     gather_current_term_data
   INNER JOIN
     term_ids
   ON
-    term_ids.GAS_Name = CONCAT(gather_current_term_data.term_to_create, " ", gather_current_term_data.year_to_create, " ", "(", calendar_type_c,")") ),
+    term_ids.gas_start_date = gather_current_term_data.start_date_of_term_to_create and gather_current_term_data.school_calendar = term_ids.academic_calendar_category_c ),
   filter_data AS (
   SELECT
     *
@@ -75,8 +76,11 @@ WITH
     join_term_ids
   WHERE
     current_gas_name != gas_name_to_create ),
+  
   prep_data_for_upload AS (
+  
   SELECT
+  gas_name_to_create,
     -- These fields are static
     contact_id,
     college_track_status_c,
@@ -136,113 +140,42 @@ CASE
     cc_advisor_at_user_id_c
   END
     AS cc_advisor_at_user_id_c,
-    -- These are the advising rubric fields. They are null for non-active students
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    financial_aid_package_c
-  END
-    AS financial_aid_package_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    free_checking_account_c
-  END
-    AS free_checking_account_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    e_fund_c
-  END
-    AS e_fund_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    repayment_plan_c
-  END
-    AS repayment_plan_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    loan_exit_c
-  END
-    AS loan_exit_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    academic_networking_50_cred_c
-  END
-    AS academic_networking_50_cred_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    academic_networking_over_50_credits_c
-  END
-    AS academic_networking_over_50_credits_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    extracurricular_activity_c
-  END
-    AS extracurricular_activity_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    finding_opportunities_75_c
-  END
-    AS finding_opportunities_75_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    resume_cover_letter_c
-  END
-    AS resume_cover_letter_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    career_counselor_25_credits_c
-  END
-    AS career_counselor_25_credits_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    career_field_2550_credits_c
-  END
-    AS career_field_2550_credits_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    resources_2550_credits_c
-  END
-    AS resources_2550_credits_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    internship_5075_credits_c
-  END
-    AS internship_5075_credits_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    post_graduate_plans_5075_creds_c
-  END
-    AS post_graduate_plans_5075_creds_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    post_graduate_opportunities_75_cred_c
-  END
-    AS post_graduate_opportunities_75_cred_c,
-    CASE
-      WHEN college_track_status_c != '15A' THEN NULL
-    ELSE
-    alumni_network_75_credits_c
-  END
-    AS alumni_network_75_credits_c,
+------------------ ADVISING RUBRIC SECTION ------------------     
+-- Details are found here: 
+-- https://docs.google.com/spreadsheets/d/1xoz7mKWl8U1wyVSAz4mcsbE7I86rBRXwUawYdcfpcEY
+
+-- FIELDS THAT CARRY OVER EVERY TERM
+    repayment_plan_c,
+    repayment_policies_c,
+    loan_exit_c,
+    academic_networking_50_cred_c,
+    academic_networking_over_50_credits_c,
+    exploration_career_c,
+    internship_5075_credits_c,
+    post_graduate_opportunities_75_cred_c,
+    interested_in_graduate_school_50_credits_c,
+    alumni_network_75_credits_c,
+
+-- FIELDS THAT RESET EVERY NEW ACADEMIC YEAR
+    case when term_to_create = 'Fall' then null
+    else financial_aid_package_c end as financial_aid_package_c,
+
+
+
+
+------------------ END ADVISING RUBRIC SECTION ------------------
+
     '01246000000RNnHAAW' as record_type_id
   FROM
-    filter_data )
+    filter_data ),
+
+    check_record_count as (
+      select gas_name_to_create, count(contact_id) as n_students, count(*) as n_records
+      from prep_data_for_upload
+      group by 1
+    )
 SELECT
   *
 FROM
   prep_data_for_upload
+  
